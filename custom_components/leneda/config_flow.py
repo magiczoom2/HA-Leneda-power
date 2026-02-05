3c04448ade02070acd7d6e72eb6ea6427239d3a8
magiczoom2 <gritty.push_8y@icloud.com>
2026-02-05 22:36:26 +0100

    Allow multiple entries

--- custom_components/leneda/config_flow.py
+++ custom_components/leneda/config_flow.py
@@ -11,9 +11,10 @@ class LenedaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            await self.async_set_unique_id(user_input[CONF_METERING_POINT])
+            unique_id = f"{user_input[CONF_METERING_POINT]}_{user_input[CONF_OBIS_CODE]}"
+            await self.async_set_unique_id(unique_id)
             self._abort_if_unique_id_configured()
-            return self.async_create_entry(title=f"Leneda {user_input[CONF_METERING_POINT]}", data=user_input)
+            return self.async_create_entry(title=f"Leneda {unique_id}", data=user_input)
 
         return self.async_show_form(
             step_id="user",
